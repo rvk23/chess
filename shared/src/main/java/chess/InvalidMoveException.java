268dce98b5ee0cac4ec1ff22aea647833440dfd7
rvk23 <rvk23@byu.edu>
2024-06-28 19:31:45 -0600

    Added starter code and changed the class imports and packages, because they did not line up when I had downloaded the starter code. I assume that was part of the project.

---
+++ shared/src/main/java/chess/InvalidMoveException.java
@@ -0,0 +1,13 @@
+package shared.src.main.java.chess;
+
+/**
+ * Indicates an invalid move was made in a game
+ */
+public class InvalidMoveException extends Exception {
+
+    public InvalidMoveException() {}
+
+    public InvalidMoveException(String message) {
+        super(message);
+    }
+}
