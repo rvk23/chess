17624cd0c36eeb528a85339c8b4892ccc67dd9f3
rvk23 <rylanvk22@gmail.com>
2025-02-26 15:14:49 -0700

    put in UserDAO test and code to pass

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,4 +1,22 @@
 package dataaccess;
 
+import model.UserData;
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserDAO {
+
+    private final Map<String,UserData> users = new HashMap<>();
+
+    public void addUser(UserData user) {
+        users.put(user.username(), user);
+    }
+
+    public UserData getUser(String username) {
+        return users.get(username);
+    }
+
+    public void clear() {
+        users.clear();
+    }
 }
