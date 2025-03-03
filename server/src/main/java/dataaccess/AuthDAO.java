8e0baaafa2025b1ab32b641b36fdd287d71d1902
rvk23 <rylanvk22@gmail.com>
2025-03-03 11:09:36 -0700

    Auth DAO code and test

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,4 +1,25 @@
 package dataaccess;
 
+import model.AuthData;
+import java.util.HashMap;
+import java.util.Map;
+
 public class AuthDAO {
+    private final Map<String, AuthData> authTokens = new HashMap<>();
+
+    public void createAuth(String authToken, String username) {
+        authTokens.put(authToken, new AuthData(authToken, username));
+    }
+
+    public AuthData getAuth(String authToken) {
+        return authTokens.get(authToken);
+    }
+
+    public void deleteAuth(String authToken) {
+        authTokens.remove(authToken);
+    }
+
+    public void clear() {
+        authTokens.clear();
+    }
 }
