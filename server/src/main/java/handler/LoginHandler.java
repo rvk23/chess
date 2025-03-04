4e53544a991eff702d05f9905dc0ffbc1fc70b4f
rvk23 <rylanvk22@gmail.com>
2025-03-03 21:35:58 -0700

    Added a login handler and test

---
+++ server/src/main/java/handler/LoginHandler.java
@@ -0,0 +1,34 @@
+package handler;
+
+import com.google.gson.Gson;
+import model.AuthData;
+import model.UserData;
+import service.UserService;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class LoginHandler implements Route {
+    private final UserService userService;
+    private final Gson gson = new Gson();
+
+    public LoginHandler(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public Object handle(Request req, Response res) {
+        try {
+            UserData user = gson.fromJson(req.body(), UserData.class);
+            AuthData auth = userService.login(user.username(), user.password());
+            res.status(200);
+            // return JSON object if work
+            return gson.toJson(auth);
+        }
+        catch (Exception e) {
+            // if doesn't work
+            res.status(401);
+            return gson.toJson("Wrong Login");
+        }
+    }
+}
---
+++ server/src/test/java/passoff/server/LoginHandlerTest.java
@@ -0,0 +1,104 @@
+package passoff.server;
+
+import com.google.gson.Gson;
+import dataaccess.AuthDAO;
+import dataaccess.UserDAO;
+import model.AuthData;
+import model.UserData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import handler.LoginHandler;
+import service.UserService;
+import spark.Request;
+import spark.Response;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LoginHandlerTest {
+    private LoginHandler handler;
+    private UserService userService;
+    private Gson gson;
+
+    @BeforeEach
+    void setUp() {
+        UserDAO userDAO = new UserDAO();
+        AuthDAO authDAO = new AuthDAO();
+        userService = new UserService(userDAO, authDAO);
+        handler = new LoginHandler(userService);
+        gson = new Gson();
+    }
+
+    @Test
+    void handle_Success() {
+        // register a user first
+        UserData user = new UserData("user", "password", "abc123@test.com");
+        userService.register(user);
+
+
+        String requestBody = gson.toJson(new UserData(user.username(), user.password(), null));
+
+        // create request and response
+        TestRequest req = new TestRequest(requestBody);
+        TestResponse res = new TestResponse();
+
+
+        String jsonResponse = (String) handler.handle(req, res);
+
+
+        AuthData response = gson.fromJson(jsonResponse, AuthData.class);
+
+
+        assertNotNull(response.authToken());
+        assertEquals("testUser", response.username());
+    }
+
+    @Test
+    void handle_Fail_InvalidCredentials() {
+        // login request to JSON
+        String requestBody = gson.toJson(new UserData("wrongUser", "wrongPass", null));
+
+
+        TestRequest req = new TestRequest(requestBody);
+        TestResponse res = new TestResponse();
+
+
+        String jsonResponse = (String) handler.handle(req, res);
+
+
+        String expectedResponse = gson.toJson("Wrong Login");
+
+
+
+        assertEquals(expectedResponse, jsonResponse);
+        assertEquals(401, res.status());
+    }
+
+
+    private static class TestRequest extends Request {
+        private final String body;
+
+        TestRequest(String body) {
+            this.body = body;
+        }
+
+        @Override
+        public String body() {
+            return body;
+        }
+    }
+
+
+    private static class TestResponse extends Response {
+        private int status;
+
+        @Override
+        public void status(int statusCode) {
+            this.status = statusCode;
+        }
+
+        @Override
+        public int status() {
+            return status;
+        }
+    }
+}
