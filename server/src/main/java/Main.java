554de4c7932514bfe969e50bc5bb613ca13737cc
rvk23 <rylanvk22@gmail.com>
2025-03-10 13:45:17 -0600

    getting ready for 4

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -3,6 +3,7 @@ import server.Server;
 import chess.*;
 
 // test server
+// add
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
