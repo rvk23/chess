d66989fe8d10b393794eb0b30280a891ab8a0cf5
rvk23 <rylanvk22@gmail.com>
2025-03-28 02:24:19 -0600

    changed what it said when joining game

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -8,8 +8,7 @@ import ui.PostloginUI;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        System.out.println("♕ 240 Chess Client");
 
         int port = 8080;
         ServerFacade facade = new ServerFacade(port);
