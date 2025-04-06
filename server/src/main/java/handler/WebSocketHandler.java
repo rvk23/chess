a3b443c4ccfee9159bcb4c894de6ff2ba3a213b8
rvk23 <rylanvk22@gmail.com>
2025-04-06 15:05:23 -0600

    setting up web socket commands chosen

--- server/src/main/java/handler/WebSocketHandler.java
+++ server/src/main/java/handler/WebSocketHandler.java
@@ -25,11 +25,54 @@ public class WebSocketHandler {
     @OnMessage
     public void onMessage(String message, Session session) {
         System.out.println("Received message: " + message);
+
+        try {
+            UserGameCommand command = gson.fromJson(message, UserGameCommand.class);
+
+            switch (command.getCommandType()) {
+                case CONNECT -> handleConnect(command, session);
+                case MAKE_MOVE -> handleMakeMove(command, session);
+                case LEAVE -> handleLeave(command, session);
+                case RESIGN -> handleResign(command, session);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            sendError(session, "Invalid WebSocket message: " + ex.getMessage());
+        }
     }
+
+    private void sendError(Session session, String errorMessage) {
+        try {
+            ServerMessage errorMessageObj = new ServerMessage(ServerMessage.ServerMessageType.ERROR);
+            session.getBasicRemote().sendText(gson.toJson(errorMessageObj));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 
     @OnClose
     public void onClose(Session session) {
         sessions.remove(session);
         System.out.println("Closed connection: " + session.getId());
     }
+
+
+
+    private void handleConnect(UserGameCommand command, Session session) {
+        // stuff
+    }
+
+    private void handleMakeMove(UserGameCommand command, Session session) {
+        // stuff
+    }
+
+    private void handleLeave(UserGameCommand command, Session session) {
+        // stuff
+    }
+
+    private void handleResign(UserGameCommand command, Session session) {
+        // stuff
+    }
+
 }
