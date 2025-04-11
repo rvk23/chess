cb20f7773d331913c2c9a98f868c13c66ee54dfe
rvk23 <rylanvk22@gmail.com>
2025-04-11 09:48:01 -0600

    Adding constructors to Connection

--- shared/src/main/java/websocket/ConnectionManager.java
+++ shared/src/main/java/websocket/ConnectionManager.java
@@ -3,7 +3,19 @@ package websocket;
 import org.eclipse.jetty.websocket.api.Session;
 import java.util.*;
 
+import websocket.messages.Connection;
+import websocket.messages.ServerMessage;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+
+
 public class ConnectionManager {
     // more like petshop
+    private final Map<String, Connection> connections = new ConcurrentHashMap<>();
+    private final Gson gson = new Gson();
+
+
+
 
 }
--- shared/src/main/java/websocket/messages/Connection.java
+++ shared/src/main/java/websocket/messages/Connection.java
@@ -1,4 +1,20 @@
 package websocket.messages;
 
+import org.eclipse.jetty.websocket.api.Session;
+import java.io.IOException;
+
 public class Connection {
+    public final String username;
+    public final Session session;
+    public final int gameID;
+
+    public Connection(String username, Session session, int gameID) {
+        this.username = username;
+        this.session = session;
+        this.gameID = gameID;
+    }
+
+    public void send(String message) throws IOException {
+        session.getRemote().sendString(message);
+    }
 }
